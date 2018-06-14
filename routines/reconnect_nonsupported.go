935b484947b633a2db6970fb92bfa296ad51f6fa
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-14 18:16:27 +0300

    Fixing the error: https://travis-ci.org/rfjakob/gocryptfs/jobs/392302251

--- routines/reconnect_nonsupported.go
+++ routines/reconnect_nonsupported.go
@@ -3,10 +3,12 @@
 package cryptoWalletRoutines
 
 import (
+	"fmt"
 	I "github.com/xaionaro-go/cryptoWallet/interfaces"
 	"log"
 )
 
 func USBHIDReconnect(parent I.USBHIDWallet) error {
 	log.Panic("cryptowallets are not supported on this platform :(")
+	return fmt.Errorf("not supported")
 }
