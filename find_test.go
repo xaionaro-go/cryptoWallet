992a117b245e40c42c15e681e0db472887413897
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-21 21:33:20 +0300

    Polishing the example

--- find_test.go
+++ find_test.go
@@ -1,14 +1,9 @@
 package cryptoWallet
 
-import (
-	"testing"
+/* // Using a pinentry:
 
-	"github.com/xaionaro-go/pinentry"
-)
-
-var (
-	pinentryClient pinentry.PinentryClient
-)
+import "github.com/xaionaro-go/pinentry"
+var pinentryClient pinentry.PinentryClient
 
 func getPin(title, description, ok, cancel string) ([]byte, error) {
 	pinentryClient.SetPrompt(title)
@@ -24,8 +19,17 @@ func getConfirm(title, description, ok, cancel string) (bool, error) {
 	pinentryClient.SetOK(ok)
 	pinentryClient.SetCancel(cancel)
 	return pinentryClient.Confirm(), nil
+}*/
+
+// Just a placeholders:
+func getPin(title, description, ok, cancel string) ([]byte, error) {
+	return []byte{}, nil
+}
+func getConfirm(title, description, ok, cancel string) (bool, error) {
+	return false, nil
 }
 
+// To do not write "if" everytime
 func checkError(err error) {
 	if err != nil {
 		panic(err)
@@ -33,35 +37,29 @@ func checkError(err error) {
 }
 
 func ExampleFindAny() {
+	// Getting a wallet
 	wallet := FindAny()
 	if wallet == nil {
 		return
 	}
 
-	err := wallet.Ping()
-	checkError(err)
-
-	pinentryClient, err = pinentry.NewPinentryClient()
-	checkError(err)
-
+	// Setting required handlers
 	wallet.SetGetPinFunc(getPin)
 	wallet.SetGetConfirmFunc(getConfirm)
 
+	// Setting a key to check
 	masterKey := []byte("some key here")
 
+	// Encrypting it. The encrypted value will be of length 16 bytes (instead of 13 bytes).
 	encryptedMasterKey, err := wallet.EncryptKey(`m/3'/14'/15'`, masterKey, []byte{}, "aWalletKeyName")
 	checkError(err)
 
+	// Decrypting it back. Now it will be of length 16 bytes (instead of 13 bytes).
 	decryptedMasterKey, err := wallet.DecryptKey(`m/3'/14'/15'`, encryptedMasterKey, []byte{}, "aWalletKeyName")
 	checkError(err)
 
-	if string(masterKey) != string(decryptedMasterKey[:len(masterKey)]) {
-		return
+	// ATM, if PIN was correct then: string(masterKey) == string(decryptedMasterKey[:len(masterKey)])
+	if string(decryptedMasterKey[:len(masterKey)]) != string(masterKey) {
+		// Something is wrong
 	}
-
-	return
-}
-
-func TestFindAny(t *testing.T) {
-	ExampleFindAny()
 }
