ee1f334df25ec346d88b8adea33ae4736b72d3f7
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-21 21:53:54 +0300

    added trezorLuks to .travis-check-backdeps.sh

--- .travis-check-backdeps.sh
+++ .travis-check-backdeps.sh
@@ -3,3 +3,7 @@
 go get github.com/rfjakob/gocryptfs
 cd "$(go env GOPATH)/src/github.com/rfjakob/gocryptfs"
 ./crossbuild.bash
+
+go get github.com/xaionaro-go/trezorLuks
+cd "$(go env GOPATH)/src/github.com/xaionaro-go/trezorLuks"
+go build
