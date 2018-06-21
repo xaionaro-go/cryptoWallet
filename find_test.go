6f7538cc5a0358fe131cdade042c27adbe36e86d
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-21 21:46:08 +0300

    Added an example for Find()

--- find_test.go
+++ find_test.go
@@ -1,8 +1,19 @@
 package cryptoWallet
 
-/* // Using a pinentry:
+import (
+	"github.com/xaionaro-go/cryptoWallet/vendors"
+)
 
+const (
+	dummyKey          = "example-example-"
+	nonce             = "" // the "nonce" is optional, so it's empty for now
+	keyName           = "cryptoWalletExample"
+	keyDerivationPath = `m/11019'/0'/0'/0'`
+)
+
+/* // Using a pinentry:
 import "github.com/xaionaro-go/pinentry"
+
 var pinentryClient pinentry.PinentryClient
 
 func getPin(title, description, ok, cancel string) ([]byte, error) {
@@ -23,23 +34,22 @@ func getConfirm(title, description, ok, cancel string) (bool, error) {
 
 // Just a placeholders:
 func getPin(title, description, ok, cancel string) ([]byte, error) {
+	// Some handler here that will return valid PIN-code
 	return []byte{}, nil
 }
 func getConfirm(title, description, ok, cancel string) (bool, error) {
+	// Some handler here that will return true or false
 	return false, nil
 }
+func doSomething(...interface{}) {}
 
-// To do not write "if" everytime
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
+// An example for FindAny()
 func ExampleFindAny() {
 	// Getting a wallet
 	wallet := FindAny()
 	if wallet == nil {
+		// The device wasn't found
+		// Process the error here
 		return
 	}
 
@@ -52,14 +62,85 @@ func ExampleFindAny() {
 
 	// Encrypting it. The encrypted value will be of length 16 bytes (instead of 13 bytes).
 	encryptedMasterKey, err := wallet.EncryptKey(`m/3'/14'/15'`, masterKey, []byte{}, "aWalletKeyName")
-	checkError(err)
+	if err != nil {
+		// Cannot encrypt the key. For example, it may happen if wrong PIN code was passed to the device
+		// Process error here
+		return
+	}
 
 	// Decrypting it back. Now it will be of length 16 bytes (instead of 13 bytes).
 	decryptedMasterKey, err := wallet.DecryptKey(`m/3'/14'/15'`, encryptedMasterKey, []byte{}, "aWalletKeyName")
-	checkError(err)
+	if err != nil {
+		// Cannot decrypt the key.
+		// Process error here
+		return
+	}
 
 	// ATM, if PIN was correct then: string(masterKey) == string(decryptedMasterKey[:len(masterKey)])
 	if string(decryptedMasterKey[:len(masterKey)]) != string(masterKey) {
 		// Something is wrong
 	}
+}
+
+// An example for Find()
+//
+// This example is adapted from gocryptfs
+func ExampleFind() {
+	// This example shows how to generate a deterministic key using "Trezor One"
+
+	// Find all trezor devices
+	trezors := Find(Filter{
+		VendorID:   &[]uint16{vendors.GetVendorID("satoshilabs")}[0],
+		ProductIDs: []uint16{1 /* Trezor One */},
+	})
+
+	// For example we require to one and only one trezor device to be connected.
+	if len(trezors) == 0 {
+		// Trezor device is not found.
+		// Process the error here
+		return
+	}
+	if len(trezors) > 1 {
+		// It's more than one Trezor device connected.
+		// Process the error here
+		return
+	}
+
+	// Using the first found device
+	trezor := trezors[0]
+
+	// Trezor may ask for PIN or Passphrase. Setting the handler for this case.
+	trezor.SetGetPinFunc(getPin)
+
+	// In some cases (like lost connection to the Trezor device and cannot
+	// reconnect) it's required to get a confirmation from the user to
+	// retry to reconnect. Setting the handler for this case.
+	trezor.SetGetConfirmFunc(getConfirm)
+
+	// To generate a deterministic key we trying to decrypt our
+	// predefined constant key using the Trezor device. The resulting key
+	// will depend on next variables:
+	// * the Trezor master key;
+	// * the passphrase (passed to the Trezor).
+	//
+	// The right key will be received only if both values (mentioned
+	// above) are correct.
+	//
+	// Note:
+	// Also the resulting key depends on this values (that we defined as
+	// constants above):
+	// * the key derivation path;
+	// * the "encrypted" key;
+	// * the nonce;
+	// * the key name.
+	key, err := trezor.DecryptKey(keyDerivationPath, []byte(dummyKey), []byte(nonce), keyName)
+	if err != nil {
+		// Cannot decrypt the key. For example, it may happen if wrong PIN code was passed to the device
+		// Process the error here
+		return
+	}
+
+	// Everything ok: key - is the wanted key
+	doSomething(key)
+	return
 }
