2119a894d226cd179887e380dd4363ff4a3f7b5f
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-07-05 23:39:53 +0300

    go fmt

--- internal/wallets/satoshilabs/trezor/trezor_unsupported.go
+++ internal/wallets/satoshilabs/trezor/trezor_unsupported.go
@@ -20,31 +20,38 @@ type TrezorBase struct {
 func (trezor *TrezorBase) SetHIDDevice(device hid.Device) {
 	log.Panic(errors.ErrNotSupportedPlatform)
 }
+
 // Reset -- see trezor.go
 func (trezor *TrezorBase) Reset() error {
 	return errors.ErrNotSupportedPlatform
 }
+
 // Ping -- see trezor.go
 func (trezor *TrezorBase) Ping() error {
 	return errors.ErrNotSupportedPlatform
 }
+
 // Reconnect -- see trezor.go
 func (trezor *TrezorBase) Reconnect() error {
 	return errors.ErrNotSupportedPlatform
 }
+
 // CheckConnection -- see trezor.go
 func (trezor *TrezorBase) CheckConnection() error {
 	return errors.ErrNotSupportedPlatform
 }
+
 // CipherKeyValue -- see trezor.go
 func (trezor *TrezorBase) CipherKeyValue(path string, isToEncrypt bool, keyName string, data, iv []byte, askOnEncode, askOnDecode bool) ([]byte, messages.MessageType) {
 	log.Panic(errors.ErrNotSupportedPlatform)
 	return nil, messages.MessageType_MessageType_Failure
 }
+
 // EncryptKey -- see trezor.go
 func (trezor *TrezorBase) EncryptKey(path string, decryptedKey []byte, nonce []byte, trezorKeyname string) ([]byte, error) {
 	return nil, errors.ErrNotSupportedPlatform
 }
+
 // DecryptKey -- see trezor.go
 func (trezor *TrezorBase) DecryptKey(path string, encryptedKey []byte, nonce []byte, trezorKeyname string) ([]byte, error) {
 	return nil, errors.ErrNotSupportedPlatform
